a6708fac8943f6d624fdcdfa529ecef4d89d0042
Walter Yan <124652920+walteryanubc@users.noreply.github.com>
2023-02-08 20:55:11 -0800

    Update corpora.py

--- corpora.py
+++ corpora.py
@@ -1,4 +1,4 @@
 import subprocess
-cmd = ['python -m textblob.download_corpora']
+cmd = ['python==3.9', '-m', 'textblob.download_corpora']
 subprocess.run(cmd)
 print("corpora downloaded successfully")
