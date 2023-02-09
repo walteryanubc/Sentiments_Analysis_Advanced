c46df1d620c3e3a459bccd1767e4c7cb9d76769b
Walter Yan <124652920+walteryanubc@users.noreply.github.com>
2023-02-09 11:55:43 -0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pandas as pd
 import streamlit as st
 nltk.download('stopwords')
 
-improt requests
+import requests
 url = "http://nltk.org/data.html"
 r = requests.get(url, allow_redirects=True)
 
