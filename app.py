790d4abf2210929d8cbe93eb5256e2c0d070ddd1
Walter Yan <124652920+walteryanubc@users.noreply.github.com>
2023-02-09 14:31:03 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# load dependencies
 from keras import backend as K
 from tensorflow.keras.models import Model, load_model
 import streamlit as st
@@ -18,24 +17,18 @@ from nltk.stem.wordnet import WordNetLemmatizer
 import pandas as pd
 import numpy as np
 
+MODEL_PATH = r"model_LSTM.h5"
+
+max_words = 500
+max_len = 1000
+EMBEDDING_DIM = 32
+tokenizer_file = 'tokenizer_LSTM.pkl'
 wordnet = WordNetLemmatizer()
 regex = re.compile('[%s]' % re.escape(string.punctuation))
-EMBEDDING_DIM = 32
 
-# path of the model
-MODEL_PATH = r"model_LSTM.h5"
-# maximize number of the allowed word in an input
-max_words = 500
-# shape of input data passed for prediction
-max_len = 1000
-# path of tokenizer file
-tokenizer_file = r"tokenizer_LSTM.pkl"
-
-# load tokenizer
 with open(tokenizer_file,'rb') as handle:
     tokenizer = pickle.load(handle)
-    
-# apply text cleaning to input data
+
 def text_cleaning(line_from_column):
     text = line_from_column.lower()
     # Replacing the digits/numbers
@@ -44,22 +37,21 @@ def text_cleaning(line_from_column):
     words = [w for w in text if w not in stopwords.words("english")]
     # apply stemming
     words = [Word(w).lemmatize() for w in words]
-    # merge words
+    # merge words 
     words = ' '.join(words)
     return text
 
-# load the sentiment analysis model
 @st.cache(allow_output_mutation=True)
 def Load_model():
     model = load_model(MODEL_PATH)
-    model.summary() # included making it visible when the model is reloaded
+    model.summary() # included to make it visible when model is reloaded
     session = K.get_session()
     return model, session
 
 if __name__ == '__main__':
-    st.title('Political Threads Sentiment Classification App')
+    st.title('Political Reddit Threads Sentiment Classification app')
     st.write('A simple sentiment analysis classification app')
-    st.subheader('Input the Thread below')
+    st.subheader('Input the Reddit Thread below')
     sentence = st.text_area('Enter your thread here',height=200)
     predict_btt = st.button('predict')
     model, session = Load_model()
@@ -72,10 +64,11 @@ if __name__ == '__main__':
         data = pad_sequences(sequences, maxlen =  max_len)
         # st.info(data)
         prediction = model.predict(data)
+
         prediction_prob_negative = prediction[0][0]
         prediction_prob_neutral = prediction[0][1]
         prediction_prob_positive= prediction[0][2]
-        # prediction classes
+
         prediction_class = prediction.argmax(axis=-1)[0]
         print(prediction.argmax())
         st.header('Prediction using LSTM model')
